6b118fbaaa4ecf176651c23613b299d1f3669a43
milagros alvarez <milagrosalvarez2604@gmail.com>
2024-09-25 10:47:45 -0300

    agrego fuentes y arreglo el footer

--- TP2/EntregaFinal/js/script.js
+++ TP2/EntregaFinal/js/script.js
@@ -1,6 +1,1 @@
-const menu = document.querySelector(".menu");
-const menu_desplegable = document.querySelector(".btn_burger_menu");
-menu_desplegable.addEventListener("click", ()=>{
-    menu.classList.toggle("visible");
-    menu_desplegable.classList.toggle("efecto");
-})
+
