97f4a7cb02a9e2c1a1d0f21a88e4140489b01ce0
Ana Bellinzona <anabellinzona@gmail.com>
2024-10-07 09:07:59 -0300

    pusheo actualizaciones de carrusel

--- TP2/EntregaFinal/js/script.js
+++ TP2/EntregaFinal/js/script.js
@@ -1,3 +1,4 @@
+/* MENÚ */
 const burger_menu = document.querySelector('#burger_menu');
 const profile_menu = document.querySelector('#profile_menu');
 const btn_burger_menu = document.querySelector('#btn_burger_menu');
@@ -13,6 +14,7 @@ btn_profile_menu.addEventListener("click", ()=>{
     profile_menu.classList.toggle("invisible");
 })
 
+/* FORMS */
 const button_open_popover = document.querySelector("#show_popover");
 const button_close_popover = document.querySelector(".close_popover");
 const div_popover = document.querySelector("#id_popover");
@@ -62,5 +64,59 @@ signin.addEventListener('click', (event)=>{
     alert("Welcome user 12234");
     signin_form.classList.remove("visible");
     signin_form.classList.add("invisible");
+})
+
+/* CARRUSEL */
+
+let carousels = document.querySelectorAll('.carousel'); //selecciona todos los elementos con la clase carousel.
+
+carousels.forEach((carousel) =>{ //itera sobre cada carrusel para aplicar la lógica interna a c/u
+    let btn_izq = carousel.querySelector('.btn_flecha_izq_carousel');
+    let btn_der = carousel.querySelector('.btn_flecha_derecha_carousel');
+    let carousel_juegos = carousel.querySelector('.carousel_juegos');
+    let card = carousel.querySelectorAll('.card');
+
+    let cantidad_pixeles_movimiento = 1200; //define la cantidad de píxeles que el carrusel se desplaza cuando se presiona el botón.
+    let cantidad_total_pixeles = card.length * 209; //tamaño total del contenedor de tarjetas en base a la cantidad de tarjetas y el tamaño individual de cada una.
+    let movimiento_pixeles_actual = carousel_juegos.clientWidth; //cantidad de píxeles que se ha movido el contenedor hasta el momento. Se inicializa con el ancho del contenedor.
+
+    btn_izq.addEventListener("click", () =>{
+        movimiento_pixeles_actual -= cantidad_pixeles_movimiento;
+
+        carousel_juegos.scrollBy({
+            left: -cantidad_pixeles_movimiento,
+            behavior: 'smooth'
+        })
+        if(btn_der.style.display == "none"){
+            btn_der.style.display = "block";
+        }
+
+        if(carousel_juegos.scrollLeft <= 0){ //scrollLeft: cantidad de píxeles que se movieron de manera horizontal
+            movimiento_pixeles_actual = carousel_juegos.clientWidth;
+            btn_izq.style.display = "none";
+        }
+    })
+
+    btn_der.addEventListener("click", () =>{
+        movimiento_pixeles_actual += cantidad_pixeles_movimiento;
+
+        carousel_juegos.scrollBy({
+            left: cantidad_pixeles_movimiento,
+            behavior: 'smooth'
+        })
+
+        if(btn_izq.style.display == "none"){
+            btn_izq.style.display = "block";
+        }
+
+        if(movimiento_pixeles_actual >= cantidad_total_pixeles){
+            cantidad_pixeles_movimiento = cantidad_total_pixeles - carousel_juegos.clientWidth;
+            btn_der.style.display = "none";
+        }
+
+        if(carousel_juegos.scrollLeft > 0){
+            btn_izq.style.display = "block";
+        }
+    })
 })
 
