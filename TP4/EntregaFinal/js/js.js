4c3d368591839467b626de88428f988ee023e059
milagros alvarez <milagrosalvarez2604@gmail.com>
2024-11-11 12:12:47 -0300

    iniciamos proyecto 4, hacemos header sticky y main con imagen y gradiente

---
+++ TP4/EntregaFinal/js/js.js
@@ -0,0 +1,14 @@
+// "use strict"
+
+// window.addEventListener("resize", setHeightMainPage)
+
+// setHeightMainPage()
+
+// function setHeightMainPage() {
+//     const windowWidth = window.innerWidth
+
+//     let first = windowWidth * 894 / 1280
+//     let second = windowWidth * 960 / 1280
+
+//     document.querySelector(".main_page").style.height = first + second + "px"
+// }
