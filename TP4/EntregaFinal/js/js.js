fc25d758f99aef0c66331d3102fc785d6477075d
Facundo Bravo <fbravo@alumnos.exa.unicen.edu.ar>
2024-11-12 10:15:54 -0300

    agrego personajes y objetos

--- TP4/EntregaFinal/js/js.js
+++ TP4/EntregaFinal/js/js.js
@@ -12,3 +12,21 @@ function setHeightMainPage() {
 
     document.querySelector(".main_page").style.height = first + second + "px"
 }
+
+window.addEventListener("scroll", getScroll)
+
+function getScroll(e) {
+    const scrollY = window.scrollY
+    document.querySelector("#header").classList.toggle("small", window.scrollY > 150)
+
+    if (scrollY >= 150) {
+        document.querySelector(".logo_img").style.width = "150px"
+        document.querySelector(".logo_img").style.height = "86px"
+        document.querySelector(".logo_img").style.transform = "unset"
+    } else {
+        document.querySelector(".logo_img").style.width = "550px"
+        document.querySelector(".logo_img").style.height = "320px"
+        document.querySelector(".logo_img").style.transform = "translateY(110px)"
+        document.querySelector("#header").style.background = ""
+    }
+}
